3af00152018a337fe2a3246d2ff0039e71faed2b
raghavendrasangam <raghavendrasangam0007@gmail.com>
2025-07-23 23:59:22 +0530

    layout changes

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -35,7 +35,7 @@ const Footer = () => {
                 </div>
             </div>
             <p className="pt-4 text-center text-xs md:text-sm pb-5">
-                Copyright 2024 © <a href="https://prebuiltui.com">QuickAI</a>. All Right Reserved.
+                Copyright 2025 © <a href="https://prebuiltui.com">QuickAI</a>. All Right Reserved.
             </p>
         </footer>
   )
