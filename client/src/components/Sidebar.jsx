904d6cbed589085266d42bac6c0b98d3ce3e00c9
raghavendrasangam <raghavendrasangam0007@gmail.com>
2025-07-24 23:59:42 +0530

    layout and sidebar ui changes

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,7 +1,21 @@
 import { useClerk, useUser } from '@clerk/clerk-react'
+import { Eraser, FileText, Hash, House, Icon, Image, Scissors, SquarePen, Users } from 'lucide-react'
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 
+const navItems = [
+    {to: '/ai', label: 'Dashboard', Icon: House},
+    {to: '/ai/write-article', label: 'Write Article', Icon: SquarePen},
+    {to: '/ai/blog-titles', label: 'Blog Titles', Icon: Hash},
+    {to: '/ai/generate-images', label: 'Generate Images', Icon: Image},
+    {to: '/ai/remove-background', label: 'Remove Background', Icon: Eraser},
+    {to: '/ai/remove-object', label: 'Remove Object', Icon: Scissors},
+    {to: '/ai/remove-resume', label: 'Review Resume', Icon: FileText},
+    {to: '/ai/community', label: 'Community', Icon: Users},
+
+
+]
 
 const Sidebar = ({sidebar, setSidebar}) => {
 
@@ -9,10 +23,16 @@ const Sidebar = ({sidebar, setSidebar}) => {
     const {signOut, openUserProfile} = useClerk()
 
   return (
-    <div className={`w-60 bg-white border-r border-gray-200 flex flex-col justify-between items-center max-sm:absolute top-14 bottom-0 ${sidebar ? 'translate-x-0' : 'max-sm: -translate-x-full'} transition-all duration-300 ease-in-out`}>
+    <div className={`w-60 bg-white border-r border-gray-200 flex flex-col justify-between items-center max-sm:absolute top-14 bottom-0 ${sidebar ? 'translate-x-0' : 'max-sm:-translate-x-full'} transition-all duration-300 ease-in-out`}>
       <div className='my-7 w-full'>
-        <img src={user.imageUrl} alt="user avatar" className='w-13 rounded-full mx-auto'/>
+        <img src={user.imageUrl} alt="useravatar" className='w-13 rounded-full mx-auto'/>
         <h1 className='mt-1 text-center'>{user.fullName}</h1>
+        <div>
+            {navItems.map(({to, label, Icon})=>(<NavLink key={to} to={to} end={to === '/ai'} onClick={()=> setSidebar(false)} className={({isActive})=> `px-3.5 py-2.5 flex items-center gap-3 rounded ${isActive ? 'bg-gradient-to-r from-[#3C81F6] to-[#9234EA] text-white' : ''}`}>
+                {()=>(<>
+                </>)}
+            </NavLink>))}
+        </div>
       </div>
     </div>
   )
