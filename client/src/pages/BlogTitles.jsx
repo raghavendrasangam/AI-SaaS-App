8b8b91c91fcade3d763c7134cd777b810b978690
raghavendrasangam <raghavendrasangam0007@gmail.com>
2025-08-20 17:32:01 +0530

    made frontend pages

--- client/src/pages/BlogTitles.jsx
+++ client/src/pages/BlogTitles.jsx
@@ -38,15 +38,15 @@ const BlogTitles = () => {
       <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#C341F6] to-[#8E37EB] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'><Hash className='w-5'/>Generate Title</button>
      </form>
      {/* right col */}
-     <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 max-h-[600px]'>
+     <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96'>
           <div className='flex items-center gap-3'>
-            <Edit className='w-5 h-5 text-[#4A7AFF]'/>
-            <h1 className='text-xl font-semibold'>Generated article</h1>
+            <Hash className='w-5 h-5 text-[#8E37EB]'/>
+            <h1 className='text-xl font-semibold'>Generated Titles</h1>
           </div>
           <div className='flex-1 flex justify-center items-center'>
             <div className='text-sm flex flex-col items-center gap-5 text-gray-400'>
-                 <Edit className='w-9 h-9'/>
-                 <p>Enter a topic and click "Generate article" to get started</p>
+                 <Hash className='w-9 h-9'/>
+                 <p>Enter a topic and click "Generate title" to get started</p>
             </div>
           </div>
      </div>
