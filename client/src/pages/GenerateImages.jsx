9d79c6be0525342dbe90d75dcc3283df6efc899b
raghavendrasangam <raghavendrasangam0007@gmail.com>
2025-09-19 20:01:54 +0530

    added backend and ready to deploy

--- client/src/pages/GenerateImages.jsx
+++ client/src/pages/GenerateImages.jsx
@@ -1,5 +1,11 @@
 import React, { useState } from 'react'
 import { Edit, Hash, Image, Sparkles } from 'lucide-react'
+import axios from 'axios'
+import { useAuth } from '@clerk/clerk-react';
+import toast from 'react-hot-toast';
+import Markdown from 'react-markdown';
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 const GenerateImages = () => {
 
@@ -8,9 +14,32 @@ const imageStyle = ['Realistic', 'Ghibli style', 'Anime style', 'Cartoon style',
       const [selectedStyle, setSelectedStyle] = useState('Realistic')
       const [input, setInput] = useState('')
       const [publish, setPublish] = useState(false)
+
+      const [loading, setLoading] = useState(false)
+      const [content, setContent] = useState('')
+
+      //token to authenticate API
+          const {getToken} = useAuth()
   
       const onSubmitHandler = async (e) => {
         e.preventDefault()
+        try {
+          setLoading(true)
+
+          const prompt = `Generate an image of ${input} in the style ${selectedStyle}`
+          
+          const {data} = await axios.post('/api/ai/generate-image', {prompt, publish}, {headers: {Authorization: `Bearer ${await getToken()}`}})
+
+          if(data.success) {
+            setContent(data.content)
+          }else{
+            toast.error(data.message)
+          }
+
+        } catch (error) {
+          toast.error(error.message)
+        }
+        setLoading(false)
       }
 
 
@@ -43,7 +72,11 @@ const imageStyle = ['Realistic', 'Ghibli style', 'Anime style', 'Cartoon style',
         <p className='text-sm'>Make this image Public</p>
       </div>
    
-      <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#00AD25] to-[#04FF50] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'><Image className='w-5'/>Generate Image</button>
+      <button disabled={loading} className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#00AD25] to-[#04FF50] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
+        {
+          loading ? <span className='w-4 h-4 my-1 rounded-full border-2 border-t-transparent animate-spin'></span> : <Image className='w-5'/>
+        }
+        Generate Image</button>
      </form>
      {/* right col */}
      <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96'>
@@ -51,12 +84,19 @@ const imageStyle = ['Realistic', 'Ghibli style', 'Anime style', 'Cartoon style',
             <Image className='w-5 h-5 text-[#00AD25]'/>
             <h1 className='text-xl font-semibold'>Generated Image</h1>
           </div>
-          <div className='flex-1 flex justify-center items-center'>
+          {
+            !content ? ( <div className='flex-1 flex justify-center items-center'>
             <div className='text-sm flex flex-col items-center gap-5 text-gray-400'>
                  <Image className='w-9 h-9'/>
                  <p>Enter a topic and click "Generate image" to get started</p>
             </div>
-          </div>
+          </div>) :(
+            <div className='mt-3 h-full'>
+              <img src={content} alt='image' className='w-full h-full' />
+              </div>
+          )
+          }
+         
      </div>
     </div>
   )
--- client/src/pages/RemoveBackground.jsx
+++ client/src/pages/RemoveBackground.jsx
@@ -1,15 +1,43 @@
 import { Edit, Eraser, Hash, Sparkles } from 'lucide-react'
 import React, { useState } from 'react'
+import axios from 'axios'
+import { useAuth } from '@clerk/clerk-react';
+import toast from 'react-hot-toast';
+import Markdown from 'react-markdown';
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 
 const RemoveBackground = () => {
 
-        const [input, setInput] = useState('')
-    
-        const onSubmitHandler = async (e) => {
-          e.preventDefault()
-        }
+      const [input, setInput] = useState('')
+      const [loading, setLoading] = useState(false)
+      const [content, setContent] = useState('')
+      
+    //token to authenticate API
+      const {getToken} = useAuth()
   
+      const onSubmitHandler = async (e) => {
+        e.preventDefault()
+        try {
+           setLoading(true)
+
+          const formData = new FormData()
+          formData.append('image', input)
+          
+          const {data} = await axios.post('/api/ai/remove-image-background', formData, {headers: {Authorization: `Bearer ${await getToken()}`}})
+
+          if(data.success) {
+            setContent(data.content)
+          }else{
+            toast.error(data.message)
+          }
+        } catch (error) {
+          toast.error(error.message)
+        }
+        setLoading(false)
+      }
+
 
   return (
     <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700'>
@@ -25,7 +53,11 @@ const RemoveBackground = () => {
      
       <p className='text-xs text-gray-500 font-light mt-1'>Supports JPG, PNG, and other image formats</p>
 
-      <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#F6AB41] to-[#FF4938] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'><Eraser className='w-5'/>Remove background</button>
+      <button disabled={loading} className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#F6AB41] to-[#FF4938] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
+        {
+          loading ? <span className='w-4 h-4 my-1 rounded-full border-2 border-t-transparent animate-spin'></span> :  <Eraser className='w-5'/>      
+          }
+      Remove background</button>
      </form>
      {/* right col */}
      <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96'>
@@ -33,12 +65,17 @@ const RemoveBackground = () => {
             <Eraser className='w-5 h-5 text-[#FF4938]'/>
             <h1 className='text-xl font-semibold'>Processed Image</h1>
           </div>
-          <div className='flex-1 flex justify-center items-center'>
+          {
+            !content ? (<div className='flex-1 flex justify-center items-center'>
             <div className='text-sm flex flex-col items-center gap-5 text-gray-400'>
                  <Eraser className='w-9 h-9'/>
                  <p>Upload an image and click "Remove background" to get started</p>
             </div>
-          </div>
+          </div>) :(
+            <img src={content} alt='image' className='mt-3 w-full h-full' />
+          )
+          }
+          
      </div>
     </div>
   )
